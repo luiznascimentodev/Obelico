9bb9fa52e197f6c36c062d378e581ed7c08a940b
Luiz <luiznascdev@gmail.com>
2025-04-06 15:36:49 -0300

    Adicionado funcionalidades back to top e botoes de rede social

--- js/custom.js
+++ js/custom.js
@@ -1,53 +1,68 @@
-
-  (function ($) {
-  
-  "use strict";
-
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height();
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
-        scrollTop: totalScroll
-        }, 300);
-      }
+(function ($) {
+    // MENU: Fecha o menu ao clicar em um link
+    $('.navbar-collapse a').on('click', function () {
+        $(".navbar-collapse").collapse('hide');
     });
 
-    $(window).on('scroll', function(){
-      function isScrollIntoView(elem, index) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(window).height()*.5;
-        if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
-          $(elem).addClass('active');
+    // CUSTOM LINK: Rolagem suave para seções
+    $('.smoothscroll').on('click', function (e) {
+        e.preventDefault();
+        const el = $(this).attr('href');
+        const elWrapped = $(el);
+        const headerHeight = $('.navbar').height();
+
+        scrollToDiv(elWrapped, headerHeight);
+
+        function scrollToDiv(element, navHeight) {
+            const offset = element.offset();
+            const totalScroll = offset.top - navHeight;
+
+            $('html, body').animate({
+                scrollTop: totalScroll
+            }, 300);
         }
-        if(!(elemBottom <= docViewBottom)) {
-          $(elem).removeClass('active');
-        }
-        var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
-        var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height()*.5;
-        $(MainTimelineContainer).find('.inner').css('height',MainTimelineContainerBottom+'px');
-      }
-      var timeline = $('#vertical-scrollable-timeline li');
-      Array.from(timeline).forEach(isScrollIntoView);
     });
-  
-  })(window.jQuery);
+
+    // SCROLL: Ativa elementos visíveis e ajusta a linha de progresso da timeline
+    $(window).on('scroll', function () {
+        const timelineItems = $('#vertical-scrollable-timeline li');
+        const mainTimelineContainer = $('#vertical-scrollable-timeline')[0];
+        const mainTimelineContainerBottom = mainTimelineContainer.getBoundingClientRect().bottom - $(window).height() * 0.5;
+
+        timelineItems.each(function () {
+            const elem = $(this);
+            const docViewTop = $(window).scrollTop();
+            const docViewBottom = docViewTop + $(window).height();
+            const elemTop = elem.offset().top;
+            const elemBottom = elemTop + $(window).height() * 0.5;
+
+            if (elemBottom <= docViewBottom && elemTop >= docViewTop) {
+                elem.addClass('active');
+            } else {
+                elem.removeClass('active');
+            }
+        });
+
+        $(mainTimelineContainer).find('.inner').css('height', `${mainTimelineContainerBottom}px`);
+    });
+
+    // BACK TO TOP BUTTON: Mostra/esconde e rola para o topo
+    $(document).ready(function () {
+        const backToTopBtn = $('.back-to-top-btn');
+
+        $(window).on('scroll', function () {
+            if ($(this).scrollTop() > 300) {
+                backToTopBtn.fadeIn();
+            } else {
+                backToTopBtn.fadeOut();
+            }
+        });
+
+        backToTopBtn.on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: 0 }, 300);
+        });
+    });
+})(jQuery);
 
 
