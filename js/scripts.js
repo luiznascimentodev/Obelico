7cfa63d879a6839bf56530fd9cc6c4667a321cf8
Luiz <luiznascdev@gmail.com>
2025-04-07 11:41:10 -0300

    Melhorado SEO e adicionado animação quando a pagina inicia

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Inicializar GSAP e ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+document.querySelectorAll("[data-animate]").forEach((element) => {
+    const animationType = element.getAttribute("data-animate");
+    gsap.from(element, {
+        scrollTrigger: {
+            trigger: element,
+            start: "top 80%", // Inicia a animação quando o elemento entra na viewport
+            toggleActions: "play none none none",
+        },
+        opacity: 0,
+        y: animationType === "fade-up" ? 50 : 0, // Animação de subida
+        duration: 1,
+    });
+});
